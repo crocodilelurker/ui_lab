eb50b71ad3cb25b3c6ba6090f0314c123a4bd2b1
crocodilelurker <swagatsahu556@gmail.com>
2026-05-02 13:18:58 +0530

    init frontend with shadcn and ts+swc

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className = "text-2xl text-amber-500">
+      Hello World
+      <br/>
+      <Button>Enter World</Button>
+      </div>
+  )
+}
+
+export default App
