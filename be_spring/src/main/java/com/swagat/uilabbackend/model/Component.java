588ac691e6dbf06d0dd09268a02ba7535e62b215
crocodilelurker <swagatsahu556@gmail.com>
2026-05-10 01:59:52 +0530

    backend base

---
+++ be_spring/src/main/java/com/swagat/uilabbackend/model/Component.java
@@ -0,0 +1,27 @@
+package com.swagat.uilabbackend.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "components")
+public class Component {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(columnDefinition =  "TEXT")
+    private String reactCode;
+    private String status;
+    // Setters and getters
+    public Long getId(){return this.id;}
+    public void setId(Long id){this.id=id; }
+
+    public String getTitle(){return this.title;}
+    public void setTitle(String title){this.title=title;}
+
+    public String getReactCode(){return this.reactCode;}
+    public void setReactCode(String reactCode){this.reactCode=reactCode;}
+
+    public String getStatus(){return this.status;}
+    public void setStatus(String status){this.status=status;}
+
+}
